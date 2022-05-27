e54120d43d6a4176f095d5c30461f87956f53bcb
joshrose <josh_rose@alumni.baylor.edu>
2022-05-27 14:09:55 +0300

    Hot keys can now be uploaded via CSV

--- AppExt.py
+++ AppExt.py
@@ -17,10 +17,11 @@ class AppExt(App):
                                                      ('CSV', '*.csv')), title='Choose a file')
         self.file_entry.insert(0, file)
         if len(self.file_entry.get()) > 0:
+            view_model.clear_macros()
+            view_model.read_csv(self.file_entry.get())
             self.activate_btn.configure(state='normal')
 
     def activate_command(self):
-        # TODO: Check if column headers are correct
         if self.button_text.get() == "Activate":
             view_model.set_macros(True)
             self.button_text.set("Deactivate")
--- ViewModel.py
+++ ViewModel.py
@@ -1,10 +1,23 @@
+import csv
+
+
 class ViewModel:
+    # TODO: Add builtin hot_keys
 
     def __init__(self):
         super(ViewModel, self).__init__()
         self.start_macros = False
         self.list_of_typed_chars = list()
-        self.hot_keys = {".gr": "Hello world!", ".ww": "Have a wonderful Wednesday!"}
+        self.hot_keys = {}
 
     def set_macros(self, enabled):
         self.start_macros = enabled
+
+    def clear_macros(self):
+        self.hot_keys.clear()
+
+    def read_csv(self, file):
+        data = csv.reader(open(file, "r", encoding='utf-8-sig'))
+        for row in data:
+            self.hot_keys[row[0]] = row[1]
+        print(self.hot_keys)
